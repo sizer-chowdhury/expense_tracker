71e7ddc15d9c40d4c47816a1bcd84d7a2c4bec64
mahbub-hasan-talukdar <mahbub.hasan@brainstation-23.com>
2024-08-19 14:43:48 +0600

    Merge branch 'feat/add-expense-merged-layer' of https://github.com/mahbub-hasan-talukdar/expense_tracker into feat/add-expense-merged-layer

--- lib/config/service_locator.dart
+++ lib/config/service_locator.dart
@@ -7,14 +7,21 @@ import 'package:expense_tracker/domain/use_case/generate_report_use_case.dart';
 import 'package:get_it/get_it.dart';
 
 import '../data/repository_imp/add_expense_repository_imp.dart';
+import '../data/repository_imp/add_expense_repository_imp.dart';
+import '../data/repository_imp/item_list_repository_imp.dart';
 import '../domain/repository/add_new_expense_repository.dart';
+import '../domain/repository/item_list_repository.dart';
 import '../domain/use_case/add_new_expense_use_case.dart';
+import '../domain/use_case/item_list_use_case.dart';
 import '../presentation/item_details/bloc/expense_details_bloc.dart';
 import '../presentation/items_list/bloc/item_bloc.dart';
 
 GetIt sl = GetIt.instance;
 
 Future<void> setUpServiceLocators() async {
+  sl.registerLazySingleton<ItemListRepository>(() => ItemListRepositoryImp());
+  sl.registerLazySingleton<ItemListUseCase>(() => ItemListUseCase());
+
   sl.registerFactory<ItemBloc>(() => ItemBloc());
 
   sl.registerLazySingleton<AddNewExpenseUseCase>(
