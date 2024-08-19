fa4ecaa5654b139b353dd37cd770ca1b3747392c
sizer <shahin.reza@brainstation-23.com>
2024-08-19 12:49:57 +0600

    refactor: remove unnecessary extension

--- packages/utilities/lib/extensions/date_time_extensions.dart
+++ packages/utilities/lib/extensions/date_time_extensions.dart
@@ -5,8 +5,3 @@ extension DateTimeFormatting on DateTime {
     return DateFormat('d MMM, yyyy').format(this);
   }
 }
-extension DateTimeCompare on DateTime {
-  bool isSameDate(DateTime other) {
-    return year == other.year && month == other.month && day == other.day;
-  }
-}
