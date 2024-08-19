5cb3559e2864d7cd336fe73da444dc3d9b669222
mahbub-hasan-talukdar <mahbub.hasan@brainstation-23.com>
2024-08-19 13:07:45 +0600

    feat/add-expense-data-layer: bar graph inclusion on dashboard page complete

--- lib/config/service_locator.dart
+++ lib/config/service_locator.dart
@@ -1,18 +1,14 @@
-import 'package:expense_tracker/data/repository_imp/generate_report_repo_imp.dart';
-import 'package:expense_tracker/domain/repository/generate_report_repo.dart';
-import 'package:expense_tracker/domain/use_case/generate_report_use_case.dart';
-import 'package:expense_tracker/data/repository_imp/fetch_expense_repository_imp.dart';
-import 'package:expense_tracker/domain/repository/fetch_expense_repository.dart';
-import 'package:expense_tracker/domain/use_case/fetch_expense_use_case.dart';
 import 'package:get_it/get_it.dart';
 
+import '../data/repository_imp/generate_report_repo_imp.dart';
+import '../domain/repository/generate_report_repo.dart';
+import '../domain/use_case/generate_report_use_case.dart';
 import '../data/repository_imp/add_expense_repository_imp.dart';
 import '../data/repository_imp/item_list_repository_imp.dart';
 import '../domain/repository/add_new_expense_repository.dart';
 import '../domain/repository/item_list_repository.dart';
 import '../domain/use_case/add_new_expense_use_case.dart';
 import '../domain/use_case/item_list_use_case.dart';
-import '../presentation/item_details/bloc/expense_details_bloc.dart';
 import '../presentation/items_list/bloc/item_bloc.dart';
 
 GetIt sl = GetIt.instance;
