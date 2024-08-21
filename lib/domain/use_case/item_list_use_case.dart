4d601c0e25526bf801ac5c52023519be485a2dff
sizer <shahin.reza@brainstation-23.com>
2024-08-21 12:18:56 +0600

    refactor: add missing dependency

--- lib/domain/use_case/item_list_use_case.dart
+++ lib/domain/use_case/item_list_use_case.dart
@@ -1,7 +1,7 @@
 // import '../../config/service_locator.dart';
 // import '../entity/item_entity.dart';
 // import '../repository/item_list_repository.dart';
-
+//
 // class ItemListUseCase {
 //   Future<(List<ItemEntity>?, String?)> readItems() {
 //     return sl<ItemListRepository>().readItems();
